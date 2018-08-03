56836d7674472077e5eeae2fa17dd4ac9a684333
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-02 20:59:29 -0500

    Function inserts current number at the lowest index that has an integer greater than it; effectively sorting the array

--- insertion.rb
+++ insertion.rb
@@ -6,11 +6,18 @@ def insertion_sort array
 		indxB4 = indx - 1
 
 		while indxB4 >= 0
-			p array[indxB4]
-			indxB4 -= 1	
+			if value < array[indxB4]
+				array[indxB4 + 1] = array[indxB4]
+				array[indxB4] = value
+				indxB4 -= 1
+			else
+				break	
+			end
+				
 		end
 
 	end	
+	array
 end	
 
-insertion_sort [1, 2, 3, 4, 5, 6, 7]
+p insertion_sort [42, 9, 0, 17, 6, 23, 0, 0, 2, 5, 2, 8, 8, 4, 32]
