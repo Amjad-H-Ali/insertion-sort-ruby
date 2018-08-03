aaad7c0f2e22bff1325cf3e36fb0d2caffadefa0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-02 20:49:44 -0500

    Function prints every element previous to the current element in the array

--- insertion.rb
+++ insertion.rb
@@ -3,8 +3,14 @@ p "Insertion Sort!"
 def insertion_sort array
 	for indx in 1 ... array.size
 		value = array[indx]
-		p value
+		indxB4 = indx - 1
+
+		while indxB4 >= 0
+			p array[indxB4]
+			indxB4 -= 1	
+		end
+
 	end	
 end	
 
-insertion_sort [1, 2, 3]
+insertion_sort [1, 2, 3, 4, 5, 6, 7]
